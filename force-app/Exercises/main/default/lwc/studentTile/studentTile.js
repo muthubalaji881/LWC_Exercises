149cf3fe0a687a1b72f6cc00dc8e44e66f7cd61d
muthubalaji881 <muthubalaji881@gmail.com>
2022-03-15 14:59:50 +0530

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -9,4 +9,7 @@ export default class StudentTile extends LightningElement {
         get tileSelected() {
             return this.isSelected ? "tile selected" : "tile";
             }
+            studentClick(){
+                alert(this.student.Name);
+                }
 }
